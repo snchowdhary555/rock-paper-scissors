e713b1bba059d8c1c1f44c7c8c71d95ce51cd4b3
snchowdhary555 <snchowdhary555@gmail.com>
2022-05-06 12:19:02 +0530

    Add function playRound(playerSelection,computerSelection)
    playRound(playerSelection,computerSelection) function plays a single
    round of game between player and computer and return the result.

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function computerPlay() {
   const randomChoice = choices[Math.floor(Math.random() * choices.length)];
   return randomChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  const capitalizePlayerSelection =
+    playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+  playerSelection = capitalizePlayerSelection;
+  if (
+    (computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper")
+  ) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else if (computerSelection === playerSelection) {
+    return `Match Draw! ${computerSelection} equals ${playerSelection}`;
+  } else {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
+}
